2a87e8a96c21b4458f00c934a87724b21107dc64
MSCMDD <MSCAoMe@outlook.com>
2024-01-19 17:00:11 +0800

    从Stellar1.15.0拉取代码

---
+++ scripts/generators/author.js
@@ -0,0 +1,24 @@
+/**
+ * author v1 | https://github.com/xaoxuu/hexo-theme-stellar/
+ */
+
+hexo.extend.generator.register('author', function (locals) {
+  const { site_tree, authors } = hexo.theme.config
+  var pages = []
+  for (let key of Object.keys(authors)) {
+    const author = authors[key]
+    if (author.hidden) {
+      continue
+    }
+    pages.push({
+      path: author.path,
+      layout: ['archive'],
+      data: {
+        author: author,
+        sidebar: site_tree.author.sidebar,
+        menu_id: site_tree.author.menu_id
+      }
+    })
+  }
+  return pages
+});
